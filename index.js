60b904e8b58db1c0ac2949207eb7a641b07f0b9b
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-03-03 19:17:47 -0800

    adding codes

--- index.js
+++ index.js
@@ -1,11 +1,106 @@
-const questions = [
-  
-];
+const fs = require('fs');
+const axios = require('axios');
+const inquirer = require('inquirer');
+const util = require('util');
+const puppeteer = require('puppeteer');
+const generateHTML = require('./generateHTML');
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function writeToFile(fileName, data) {
- 
+let img = '';
+let location = '';
+let gitProfile = '';
+let userBlog = '';
+let userBio = '';
+let repoNum = 0;
+let followers = 0;
+let following = 0;
+let starNum = 0;
+let color = '';
+
+// initialize
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'Input your GitHub User Name: ',
+        name: 'username'
+      },
+      {
+        type: 'input',
+        message:
+          'What color do you want to use as your theme green/red/pink/blue?',
+        name: 'color'
+      }
+    ])
+    .then(function({ username, color }) {
+      // setup axios config to get data from github api
+      const config = {
+        headers: {
+          accept: 'application/json'
+        }
+      };
+
+      // Get GitHub API data by the axios get method
+      let queryUrl = `https://api.github.com/users/${username}`;
+
+      return axios.get(queryUrl, config).then(userData => {
+        let newUrl = `https://api.github.com/users/${username}/starred`;
+
+        axios.get(newUrl, config).then(starredRepos => {
+          data = {
+            img: userData.data.avatar_url,
+            location: userData.data.location,
+            gitProfile: userData.data.html_url,
+            userBlog: userData.data.blog,
+            userBio: userData.data.bio,
+            repoNum: userData.data.public_repos,
+            followers: userData.data.followers,
+            following: userData.data.following,
+            starNum: starredRepos.data.length,
+            username: username,
+            color: color
+          };
+          console.log(data);
+          // Function call
+          generateHTML(data);
+          creatHTML(generateHTML(data));
+          generatePDF(username);
+        });
+      });
+    });
 }
 
-function init() {
+// Create index.html file using generateHTML.js
+const creatHTML = function(generateHTML) {
+  writeFileAsync('index.html', generateHTML);
+};
 
 init();
+
+// generatePDF Funcion puppeteer accoding to puppeteer guide line
+async function generatePDF(username) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // setup index.html file location
+    await page.goto(
+      './index.html'
+    );
+    await page.emulateMediaType('screen');
+
+    // setup puppeteer pdf generation options
+    await page.pdf({
+      path: `${username}.pdf`,
+      format: 'Letter',
+      printBackground: true,
+      landscape: true
+    });
+    console.log('Generated PDF sucessfully !');
+
+    await browser.close();
+  } catch (err) {
+    console.log('Oops! PDF generate error !!!');
+  }
+}
